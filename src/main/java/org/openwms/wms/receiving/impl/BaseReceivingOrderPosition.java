b5db62ff7b9a4b9001c52dbb8c92c626057e3c4b
Heiko Scherrer <hscherrer@interface21.io>
2021-09-12 23:59:13 +0200

    Worked on query all pickTU and ShippingOrders for a given pickTU

---
+++ src/main/java/org/openwms/wms/receiving/impl/BaseReceivingOrderPosition.java
@@ -0,0 +1,215 @@
+/*
+ * Copyright 2005-2021 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openwms.wms.receiving.impl;
+
+import org.ameba.integration.jpa.BaseEntity;
+import org.openwms.core.units.api.Measurable;
+import org.openwms.wms.order.OrderState;
+import org.openwms.wms.receiving.inventory.Product;
+
+import javax.persistence.CollectionTable;
+import javax.persistence.Column;
+import javax.persistence.DiscriminatorColumn;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.ForeignKey;
+import javax.persistence.Inheritance;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.MapKeyColumn;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+import java.time.ZonedDateTime;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.openwms.wms.order.OrderState.COMPLETED;
+
+/**
+ * A BaseReceivingOrderPosition.
+ * 
+ * @author Heiko Scherrer
+ */
+@Entity
+@Inheritance
+@DiscriminatorColumn(name = "C_DISC")
+@Table(name = "WMS_REC_ORDER_POSITION",
+        uniqueConstraints = @UniqueConstraint(name = "UC_ORDER_ID_POS", columnNames = { "C_ORDER_ID", "C_POS_NO" }))
+public class BaseReceivingOrderPosition extends BaseEntity implements Serializable {
+
+    @ManyToOne(optional = false, targetEntity = ReceivingOrder.class)
+    @JoinColumn(name = "C_ORDER_ID", referencedColumnName = "C_ORDER_ID", foreignKey = @ForeignKey(name = "FK_REC_POS_ORDER_ID"))
+    private ReceivingOrder order;
+
+    /** The position number is a unique index within a single {@link ReceivingOrder} instance. */
+    @Column(name = "C_POS_NO", nullable = false)
+    @NotNull
+    private Integer posNo;
+
+    /** Current position state. Default is {@value} */
+    @Enumerated(EnumType.STRING)
+    @Column(name = "C_STATE")
+    @NotNull
+    private OrderState state = OrderState.CREATED;
+
+    @org.hibernate.annotations.Type(type = "org.openwms.core.units.persistence.UnitUserType")
+    @org.hibernate.annotations.Columns(columns = {
+            @Column(name = "C_QTY_EXPECTED_TYPE", nullable = false),
+            @Column(name = "C_QTY_EXPECTED", nullable = false)
+    })
+    @NotNull
+    private Measurable quantityExpected;
+
+    @org.hibernate.annotations.Type(type = "org.openwms.core.units.persistence.UnitUserType")
+    @org.hibernate.annotations.Columns(columns = {
+            @Column(name = "C_QTY_RECEIVED_TYPE", nullable = true),
+            @Column(name = "C_QTY_RECEIVED", nullable = true)
+    })
+    private Measurable quantityReceived;
+
+    /** The ordered {@link Product} identified by it's SKU. */
+    @ManyToOne
+    @JoinColumn(name = "C_SKU", referencedColumnName = "C_SKU", foreignKey = @ForeignKey(name = "FK_REC_POS_PRODUCT"), nullable = false)
+    @NotNull
+    private Product product;
+
+    /** The business key of the expected {@code TransportUnit} that will be received. */
+    @Column(name = "C_TRANSPORT_UNIT_BK")
+    private String transportUnitBK;
+
+    /** Arbitrary detail information on this position, might by populated with ERP information. */
+    @ElementCollection
+    @CollectionTable(name = "WMS_REC_ORDER_POSITION_DETAIL",
+            joinColumns = {
+                    @JoinColumn(name = "C_ORDER_POS_PK", referencedColumnName = "C_PK")
+            },
+            foreignKey = @ForeignKey(name = "FK_REC_ORDER_DETAILS_ROP")
+    )
+    @MapKeyColumn(name = "C_KEY")
+    @Column(name = "C_VALUE")
+    private Map<String, String> details;
+
+    /** Latest date this position can be processed. */
+    @Column(name = "C_LATEST_DUE")
+    private ZonedDateTime latestDueDate;
+
+    /** Used by the JPA provider. */
+    protected BaseReceivingOrderPosition() {}
+
+    public BaseReceivingOrderPosition(Integer posNo, Measurable quantityExpected, Product product) {
+        this.posNo = posNo;
+        this.quantityExpected = quantityExpected;
+        this.product = product;
+    }
+
+    public String getTransportUnitBK() {
+        return transportUnitBK;
+    }
+
+    public ReceivingOrder getOrder() {
+        return order;
+    }
+
+    public void setOrder(ReceivingOrder order) {
+        this.order = order;
+    }
+
+    public int getPosNo() {
+        return posNo;
+    }
+
+    public OrderState getState() {
+        return state;
+    }
+
+    public void setState(OrderState state) {
+        if (state == COMPLETED) {
+            this.quantityReceived = this.quantityExpected;
+        }
+        this.state = state;
+    }
+
+    public Measurable getQuantityExpected() {
+        return quantityExpected;
+    }
+
+    public void setQuantityExpected(Measurable quantityExpected) {
+        this.quantityExpected = quantityExpected;
+    }
+
+    public Measurable getQuantityReceived() {
+        return quantityReceived;
+    }
+
+    public Measurable addQuantityReceived(Measurable quantityReceived) {
+        if (this.quantityReceived == null) {
+            this.quantityReceived = quantityReceived;
+        } else {
+            this.quantityReceived = this.quantityReceived.add(quantityReceived);
+        }
+        return this.quantityReceived;
+    }
+
+    public void setQuantityReceived(Measurable quantityReceived) {
+        this.quantityReceived = quantityReceived;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    /**
+     * Get all the details of this {@link BaseReceivingOrderPosition}.
+     *
+     * @return As Map
+     */
+    public Map<String, String> getDetails() {
+        return details == null ? Collections.emptyMap() : details;
+    }
+
+    /**
+     * Add a new detail to the {@link BaseReceivingOrderPosition}.
+     *
+     * @param key The unique key of the detail
+     * @param value The value as String
+     * @return This instance
+     */
+    public BaseReceivingOrderPosition addDetail(String key, String value) {
+        if (details == null) {
+            details = new HashMap<>();
+        }
+        details.put(key, value);
+        return this;
+    }
+
+    public ZonedDateTime getLatestDueDate() {
+        return latestDueDate;
+    }
+
+    @Override
+    public String toString() {
+        return (order == null ? "n/a" : order.getOrderId()) + "/" + posNo;
+    }
+}
