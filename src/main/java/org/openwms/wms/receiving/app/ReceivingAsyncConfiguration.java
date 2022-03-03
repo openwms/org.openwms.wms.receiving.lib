9dd97e3f4b6f338b57be33d4980406d2df4148da
Heiko Scherrer <hscherrer@interface21.io>
2022-03-03 22:36:16 +0100

    The RabbiTemplate must be extended with interceptors

--- src/main/java/org/openwms/wms/receiving/app/ReceivingAsyncConfiguration.java
+++ src/main/java/org/openwms/wms/receiving/app/ReceivingAsyncConfiguration.java
@@ -31,12 +31,14 @@ import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.amqp.support.converter.SerializerMessageConverter;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.retry.backoff.ExponentialBackOffPolicy;
 import org.springframework.retry.support.RetryTemplate;
@@ -72,9 +74,11 @@ public class ReceivingAsyncConfiguration {
         return messageConverter;
     }
 
-    @Bean
-    RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory,
-            MessageConverter messageConverter, @Autowired(required = false) RabbitTemplateConfigurable rabbitTemplateConfigurable) {
+    @Primary
+    @Bean(name = "amqpTemplate")
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory,
+            ObjectProvider<MessageConverter> messageConverter,
+            @Autowired(required = false) RabbitTemplateConfigurable rabbitTemplateConfigurable) {
         RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
         ExponentialBackOffPolicy backOffPolicy = new ExponentialBackOffPolicy();
         backOffPolicy.setMultiplier(2);
@@ -83,7 +87,7 @@ public class ReceivingAsyncConfiguration {
         RetryTemplate retryTemplate = new RetryTemplate();
         retryTemplate.setBackOffPolicy(backOffPolicy);
         rabbitTemplate.setRetryTemplate(retryTemplate);
-        rabbitTemplate.setMessageConverter(messageConverter);
+        rabbitTemplate.setMessageConverter(messageConverter.getIfUnique());
         if (rabbitTemplateConfigurable != null) {
             rabbitTemplateConfigurable.configure(rabbitTemplate);
         }
