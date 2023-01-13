e1de87b78074dd488a6449f85f3814d2106f34e9
Heiko Scherrer <hscherrer@interface21.io>
2023-01-13 23:56:17 +0100

    #23 removed unused method in Feign client

--- src/main/java/org/openwms/wms/receiving/spi/wms/inventory/ProductApi.java
+++ src/main/java/org/openwms/wms/receiving/spi/wms/inventory/ProductApi.java
@@ -3,15 +3,9 @@ package org.openwms.wms.receiving.spi.wms.inventory;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name = "wms-inventory", decode404 = true, qualifiers = "productApi")
 public interface ProductApi {
-
-    /** API version. */
-    String API_VERSION = "v1";
-    /** API root to hit Products (plural). */
-    String API_PRODUCTS = "/" + API_VERSION + "/products";
 
     /**
      * Gets {@code Product} based on {@code ProductUnit} pKey
@@ -20,13 +14,4 @@ public interface ProductApi {
      */
     @GetMapping("/v1/product/product-units/{pKey}")
     ProductVO findProductByProductUnitPkey(@PathVariable("pKey") String pKey);
-
-    /**
-     * Find and return a {@code Product}.
-     *
-     * @param bk The business key, either the SKU or the Label
-     * @return The instance
-     */
-    @GetMapping(value = API_PRODUCTS, params = {"bk"})
-    ProductVO findByLabelOrSKU(@RequestParam("bk") String bk);
 }
